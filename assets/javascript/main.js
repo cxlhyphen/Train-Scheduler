87214528b3a16a679a43dc0b51b4c779fc54105b
cxlhyphen <42073246+cxlhyphen@users.noreply.github.com>
2018-10-31 01:18:08 -0400

    finished initial layout and enabled firebase

---
+++ assets/javascript/main.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAfUJ2A5PcQcBeq2Iwl2j0a02C3jkvZ_Ng",
+    authDomain: "trains-5f87f.firebaseapp.com",
+    databaseURL: "https://trains-5f87f.firebaseio.com",
+    projectId: "trains-5f87f",
+    storageBucket: "",
+    messagingSenderId: "837841713221"
+};
+firebase.initializeApp(config);
+
