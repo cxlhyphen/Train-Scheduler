60dd751b2b805a6ada91ceb254797dea9562b8d2
cxlhyphen <42073246+cxlhyphen@users.noreply.github.com>
2018-11-02 01:45:57 -0400

    added moment.js methods

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -43,19 +43,32 @@ $("#submit").on("click", function() {
 //retireve child addeds from database and name it cSnapshot, then create td + tr for info and append onto table
 databaseRef.ref().on("child_added", function(cSnapshot) {
 
-    //grab data from firebase + log it
-    console.log("Train Name: " + cSnapshot.val().trainName);
-    console.log("Destination: " + cSnapshot.val().destination);
-    console.log("First Train: " + cSnapshot.val().firstTrain);
-    console.log("Frequency: " + cSnapshot.val().frequency);
+    var firstTrainConvert = moment(cSnapshot.val().firstTrain, "HH:mm").subtract(1, "years");
+    console.log("1st Train Converted: " + firstTrainConvert);
+
+    var currTime = moment()
+    console.log("Currently: " + currTime);
+
+    var diffTime = moment().diff(moment(firstTrainConvert), "minutes");
+    console.log("Difference: " + diffTime);
+
+    var tRemaining = diffTime % cSnapshot.val().frequency;
+    console.log("Remaining min: " + tRemaining);
+
+    var minTilTrain = cSnapshot.val().frequency - tRemaining;
+    console.log("Minutes til: " + minTilTrain);
+
+    var nextTrain = moment().add(minTilTrain, "minutes").format("HH:mm");
+    console.log("Next Train: " + nextTrain);
+
 
     //grab data from firebase and append to table
     var tableRow = $("<tr>");
     var tdName = $("<td>").text(cSnapshot.val().trainName);
     var tdDestination = $("<td>").text(cSnapshot.val().destination);
     var tdFrequency = $("<td>").text(cSnapshot.val().frequency);
-    var tdArrival = $("<td>").text("");
-    var tdMinAway = $("<td>").text("");
+    var tdArrival = $("<td>").text(nextTrain);
+    var tdMinAway = $("<td>").text(minTilTrain);
 
     tableRow.append(tdName);
     tableRow.append(tdDestination);
